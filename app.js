a674befcbf04b1b258d2a93c90e45357df246079
Camilo <iecam03@gmail.com>
2020-04-13 19:42:41 -0500

    saludame2

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/make/:nombre', (req, res) => {
+  var nombre = req.params.nombre
+  var nombreCapitalized = nombre.charAt(0).toUpperCase() + nombre.slice(1)
+  if(nombre){
+    res.send('<h1>Hola ' + nombreCapitalized +'!</h1>')
+  } else{
+    res.send('<h1>Hola ' + 'desconocido' +'!</h1>')
+  };  
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
