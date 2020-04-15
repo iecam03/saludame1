3b8bf0ca90d9d1a7b5c206ff182de122466c4c8d
Camilo <iecam03@gmail.com>
2020-04-14 19:06:22 -0500

    con error 400

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const app = express();
 app.get('/make/:nombre', (req, res) => {
   var nombre = req.params.nombre
   var nombreCapitalized = nombre.charAt(0).toUpperCase() + nombre.slice(1)
-  res.send('<h1>Hola ' + nombreCapitalized +'!</h1>')
+  if(nombre){
+    res.send('<h1>Hola ' + nombreCapitalized +'!</h1>')
+  } else{
+    res.status(404)
+  };  
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
